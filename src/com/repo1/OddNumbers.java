19bb6e051675e8f6f68690407065ef14d8106a60
Ganesh Dandekar <ganeshrajdandekar68@gmail.com>
2023-11-09 08:27:24 +0530

    New Project commit1

---
+++ src/com/repo1/OddNumbers.java
@@ -0,0 +1,16 @@
+package com.repo1;
+
+public class OddNumbers {
+	
+	public static void main(String [] args)
+	{
+		for(int i=1;i<10;i++)
+		{
+			if(i%2!=0)
+			{
+				System.out.println("Odd no: "+i);
+			}
+		}
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Ganesh Dandekar
+ *
+ */
+module BEST_JunrBatchRemoteRepo {
+}
