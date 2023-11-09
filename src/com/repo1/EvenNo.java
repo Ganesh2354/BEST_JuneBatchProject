d80b5ec2e90f80bb135deabfe88c5b7be1ada206
Ganesh Dandekar <ganeshrajdandekar68@gmail.com>
2023-11-09 08:40:09 +0530

    Even Class Added

---
+++ src/com/repo1/EvenNo.java
@@ -0,0 +1,16 @@
+package com.repo1;
+
+public class EvenNo {
+
+	public static void main(String[] args) {
+		for(int i=1;i<10;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println("Even no: "+i);
+			}
+		}
+
+	}
+
+}
